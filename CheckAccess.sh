f75dd245a576ae014c3358a13ae7868f830ad176
CJieLuo <CommAdama@outlook.com>
2019-03-03 14:04:58 +0800

    ADD CheckAccess.sh

---
+++ CheckAccess.sh
@@ -0,0 +1,28 @@
+file_name=gfwlist.acl
+times=1
+timeout=3
+proxy_port=12333
+row=1
+echo "请确定已经打开全局代理"
+export http_proxy="http://127.0.0.1:$proxy_port"
+export https_proxy="http://127.0.0.1:$proxy_port"
+rows=$(grep ^\^ $file_name | wc -l)
+grep ^\^ $file_name | while read line
+do
+	url=$(echo $line | sed "s/^.\{7\}//" | sed "s/\$$//")
+	echo "$row/$rows"
+	ping_result=$(ping -c $times -W $timeout $url | sed -n "/ms$/p")
+	if echo $ping_result | grep ", 0% packet"
+	then
+		ping_time=$(echo $ping_result | sed "s/.*\/\(.*\)\/.*/\1/")
+		echo "$url $ping_time ms" >> NoNeedToProxy.txt
+	else
+		echo "$url need proxy"
+		result_code=`curl -I -m $timeout -o /dev/null -s -w %{http_code} $url`
+		if [ $result_code -ne 200 ]
+		then
+			echo "$url $result_code" >> fail.txt
+		fi
+	fi
+	((row++))
+done
--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Copy from shadowsocksrr/shadowsocksr-android.Added some missing websites.
 绕过了gcm推送服务，避免有可能出现的gcm时断时续问题
 
 更新地址：https://raw.githubusercontent.com/CJieLuo/GFWIistforAndroidSSR/master/gfwlist.acl
+
+增加CheckAccess.sh,用于检测acl中的网站是否有效
+* 只能检测类似^(.*\.)?akamaized\.net$的格式
+* 执行前请将acl与CheckAccess.sh置于同一目录
+* 使用ping和curl来检测网站
+* 执行后在目录下生成NoNeedToProxy.txt(ping无丢包) fail.txt(curl输出除200外的http错误代码)
